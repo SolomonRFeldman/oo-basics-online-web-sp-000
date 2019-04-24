243e6247ab90de279271d1b4a986f931125cdd49
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-24 12:51:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,13 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
   def initialize(title)
     @title = title
   end
-  def title
-    @title
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-  attr_accessor :author
-  attr_accessor :page_count
-  attr_accessor :genre
 end
 
